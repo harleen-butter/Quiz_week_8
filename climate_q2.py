860ab3826b9cc8e6b6cbbc930341bf81d721b33c
harleen-butter <harleenkaur.butter@griffithuni.edu.au>
2023-09-13 21:56:08 +1000

    updated climate_q2 to load data from csv

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-years = []
-co2 = []
-temp = []
+data = pd.read_csv('climate.csv')
+
+Year = data['Year']
+CO2 = data['CO2']
+Temperature = data['Temperature']
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(Year, CO2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(Year, Temperature, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+
+plt.show()
+
+plt.savefig("co2_temp_2.png")
 
