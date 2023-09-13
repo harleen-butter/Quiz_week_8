b3cff118ef7e0c498890291f2da2522bb21ca8ca
harleen-butter <harleenkaur.butter@griffithuni.edu.au>
2023-09-13 21:47:07 +1000

    updated climate_q1 to fetch data and populate

--- climate_q1.py
+++ climate_q1.py
@@ -1,18 +1,30 @@
 import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
+import sqlite3
 
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+
+cursor.execute("SELECT Year, CO2, Temperature FROM ClimateData")
+data = cursor.fetchall()
+
+Year = [row[0] for row in data]
+CO2 = [row[1] for row in data]
+Temperature = [row[2] for row in data]
+
+conn.close()
+
+# Create subplots and plot the data
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(Year, CO2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.plot(Year, Temperature, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+
+plt.savefig("co2_temp_1.png")
+
+plt.show()
